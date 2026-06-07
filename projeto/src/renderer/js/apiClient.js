8bf0d732be9972eb8a371d06af66637b36d5b17a
marceloferreiracampos07 <junior87721727@gmail.com>
2026-06-07 01:51:19 -0300

    chore: update API_BASE_URL to production endpoint

--- projeto/src/renderer/js/apiClient.js
+++ projeto/src/renderer/js/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://projeto-estoque-desenvolvimento-desktop.onrender.com/api';
 
 const apiClient = {
   async request(endpoint, options = {}) {

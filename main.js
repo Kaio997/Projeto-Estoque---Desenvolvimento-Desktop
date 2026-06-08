e20b5bd3b3e1d42ec7255ad04d55758231bdae5d
marceloferreiracampos07 <junior87721727@gmail.com>
2026-06-08 17:37:49 -0300

    chore: remove vercel config and cleanup project for electron-only

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'projeto/src/renderer/views/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.on('window-all-closed', () => {
+      app.quit();
+    });
+  }
+});
